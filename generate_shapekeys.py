7d840ae9750becf07e1a2bdad388e94613f39311
juzza1 <juzza1@gmail.com>
2020-10-01 19:18:26 +0300

    dont generate extra shapekeys

--- generate_shapekeys.py
+++ generate_shapekeys.py
@@ -20,16 +20,14 @@ def reset_shape_keys(obj):
 def make_shape_keys(obj, rules, delete_mixers=False):
     mix_keys = rules['mix_keys']
     reset_shape_keys(obj)
-    for shape_key_name in rules['generated_shape_keys']:
-        if shape_key_name in obj.data.shape_keys.key_blocks:
+    for shape_key_name, mix_values in rules['generated_shape_keys'].items():
+        # dont generate key if we already have explicit shape key with this name
+        # or if there aren't actually any keys with value other than 0
+        if shape_key_name in obj.data.shape_keys.key_blocks or all(v == 0.0 for v in mix_values):
             continue
-        mix_values = rules['generated_shape_keys'][shape_key_name]
         for i, v in enumerate(mix_values):
-            try:
-                print("setting key {} to {}".format(mix_keys[i], v))
-                obj.data.shape_keys.key_blocks[mix_keys[i]].value = v
-            except IndexError:
-                pass
+            print("setting key {} to {}".format(mix_keys[i], v))
+            obj.data.shape_keys.key_blocks[mix_keys[i]].value = v
         print('creating new mix {}'.format(shape_key_name))
         obj.shape_key_add(name=shape_key_name, from_mix=True)
         reset_shape_keys(obj)
@@ -42,7 +40,7 @@ class GenerateShapeKeys(bpy.types.Operator):
     bl_idname = 'object.shape_key_generate'
     bl_label = "Generate shape keys from rules"
     bl_options = {'REGISTER', 'UNDO'}
-    
+
     delete_mix_keys = bpy.props.BoolProperty(default=False)
 
     def execute(self, context):
@@ -68,5 +66,5 @@ def unregister():
 # to test the add-on without having to install it.
 if __name__ == "__main__":
     register()
-    
+
     #bpy.ops.object.shape_key_generate()
